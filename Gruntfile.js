db6e0003985f94e06b155db9169b56ff71fa41f9
pasupulaphani <pasupulaphani@gmail.com>
2014-12-08 23:47:24 +0000

    fixing grunt min errros

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,10 @@ module.exports = function(grunt) {
         },
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+                mangle: {
+                    except: ['angular']
+                }
             },
             jid: {
                 files: {
