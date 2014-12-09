c13e393e95876e76a93667ac8fe8d2c450a84777
pasupulaphani <pasupulaphani@gmail.com>
2014-12-09 00:07:31 +0000

    cleaning up uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,9 +22,7 @@ module.exports = function(grunt) {
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
-                mangle: {
-                    except: ['angular']
-                }
+                mangle: false
             },
             jid: {
                 files: {
