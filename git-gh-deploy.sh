bd6885609933fb5404b826097e610dc5cf4de9b5
pasupulaphani <pasupulaphani@gmail.com>
2015-01-09 00:29:36 +0000

    deploy to gh-pages Fri Jan  9 00:29:36 GMT 2015

--- git-gh-deploy.sh
+++ git-gh-deploy.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
+
+grunt
+
+printf "\n ################### Grunt done ######################### \n"
+
+git add .
+git add -u
+
+
+printf "\n ################### git status ######################### \n"
+git status
+
+git commit -m "deploy to gh-pages `date`"
+git push origin gh-pages-gen
+
+printf "\n ################### git committed and pushed ######################### \n"
+
 if [ -z "$1" ]
 then
   deploy_folder="dist"
 else
   deploy_folder=$1
 fi
-
-grunt
 
 echo "Deploying $deploy_folder folder to GitHub Pages"
 
